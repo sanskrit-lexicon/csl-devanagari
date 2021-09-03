07a762f70ca8599c21b69a7a383d924d82532c9d
drdhaval2785 <drdhaval2785@gmail.com>
2021-09-03 12:31:03 +0530

    redo_all pulls latest csl-orig

--- scripts/redo_all.sh
+++ scripts/redo_all.sh
@@ -1,3 +1,9 @@
+echo "UPDATE CSL-ORIG REPOSITORY."
+cd ../../csl-orig
+git pull origin master
+cd ../csl-devanagari/scripts
+echo "UPDATING CSL-ORIG COMPLETED."
+echo ""
 dicts=(wil yat gst ben mw72 lan cae md mw shs ap90 mwe bor ae bur stc pwg gra pw ccs sch bop armh vcp skd inm vei pui bhs acc krm ieg snp pe pgn mci)
 for dict in ${dicts[@]};
 do
