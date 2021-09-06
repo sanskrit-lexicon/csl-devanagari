649f596f1d11cb8c4d429c8175e5b55facf89150
drdhaval2785 <drdhaval2785@gmail.com>
2021-09-06 10:22:50 +0530

    https://github.com/sanskrit-lexicon/CORRECTIONS/issues/92#issuecomment-913174476 started

---
+++ scripts/corrections_issue_92/corrections_issue_92.py
@@ -0,0 +1,43 @@
+# coding=utf-8
+"""Transfer missing data to respective dictionaries.
+    Author - Dr. Dhaval Patel
+    email - drdhaval2785@gmail.com
+    date - 06 September 2021
+    Usage - python3 corrections_issue_92.py
+"""
+import codecs
+import os
+import re
+from indic_transliteration import sanscript
+
+def update_missing():
+	corfin = codecs.open('Filling.the.missed.places.txt', 'r', 'utf-8')
+	correctionData = corfin.read()
+	corfin.close()
+	correctionData = correctionData.replace('\r\n', '\n')
+	dictwiseData = correctionData.split('------------------------\n')[:-1]
+	for dictd in dictwiseData:
+		for lin in dictd.split('\n')[:-1]:
+			m = re.search('^([a-z0-9]*).txt$', lin)
+			n = re.search('^\tLine ([0-9]+)\:\t(.*)$', lin)
+			o = re.search('^\t\t([^%].*)$', lin)
+			if m:
+				dictcode = m.group(1)
+				fin = codecs.open('../../v02/' + dictcode + '/' + dictcode + '.txt', 'r', 'utf-8')
+				data = fin.read().split('\n')
+				fin.close()
+			elif n:
+				linenum = int(n.group(1)) - 1
+				old = n.group(2)
+				new = ''
+			elif o:
+				new = o.group(1)
+				if dictcode == 'bor':
+					print(linenum)
+					print(data[linenum])
+					print(new)
+
+if __name__ == "__main__":
+	update_missing()
+
+	
